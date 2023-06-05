7b5adb96ed990643765bf009c4bfe961e89a3e5c
Ben O'Mahony <bomarni@googlemail.com>
2023-06-05 17:15:26 +0100

    adding logo instead of text

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,14 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
+import logo from '../../static/img/logo_square.jpeg'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <img src={logo} alt="Logo" />;
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
     </header>
@@ -31,7 +32,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
