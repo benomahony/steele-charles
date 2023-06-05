2e17ecff27a0ba878836315cfe71b84234bbf3bc
Ben O'Mahony <ben.omahony@infogrid.io>
2023-06-05 15:58:03 +0100

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,123 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Steele Charles',
+  tagline: 'Relationship led headhunting',
+  favicon: 'img/SC_logo.jpeg',
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-test-site.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'steele-charles', // Usually your GitHub org/user name.
+  projectName: 'steele-charles', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      stylesheets: [
+        '/css/custom.css',
+        '/pages/index.module.css'
+      ],
+      image: 'img/SC_logo.jpeg',
+      navbar: {
+        title: 'Steele Charles',
+        logo: {
+          alt: 'Steele Charles Logo',
+          src: 'img/SC_logo.jpeg',
+        },
+        items: [
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/jobs', label: 'Jobs', position: 'left'},
+          {to: 'https://www.linkedin.com/company/steele-charles-ltd/', label: 'Linkedin', position: 'left'},
+          {to: '/about', label: 'About Us', position: 'left'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'Linkedin',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+              {
+                label: 'About Us',
+                href: '/about',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Steele Charles Ltd. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'BroadRiver Assset Management',
+    Svg: require('@site/static/img/undraw_business_deal_re_up4u.svg').default,
+    description: (
+      <>
+        We hired Liam to help us find a new General Counsel and Chief Compliance Officer for our growing firm with 20 staff and US$1.5 billion under management at a particularly crucial time in the growth of our firm. As expected, Liam and his firm turned up many candidates and three, in particular, who were good fits for us. Liam was always prompt and efficient.
+But what set him apart was his fine-tuned attention to the qualitative aspects of the assignment, particularly his judgment about about which candidate would be the best fit for the culture of our organization and would work best on personal basis with us and our senior staff. The hire has turned out excellently.
+      </>
+    ),
+  },
+  {
+    title: 'Nagashima Ohno & Tsunematsu NY LLP',
+    Svg: require('@site/static/img/Nagashima.svg').default,
+    description: (
+      <>
+        Thank you for your dedicated support.
+      </>
+    ),
+  },
+  {
+    title: 'Karen Clayton',
+    Svg: require('@site/static/img/undraw_female_avatar_efig.svg').default,
+    description: (
+      <>
+        I have had the pleasure of working with Liam on two contracts in the last 18 months. Liam's ability to build empathetic relationships with both candidates and clients gives him powerful insight into the human side of recruitment. Liam invested the time to understand the details of the role requirements and the working culture of the business matching this with the skill-set, experience and values of the candidate. It truly felt like a partnership working towards the right outcome for all involved and I would highly recommend Liam for your next role whether as a client or a candidate!
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
